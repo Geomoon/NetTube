3a2dce70dea7cac9b5c7edc1e8e46aecd26d2d3c
Elvis.Vele <75845853+elvis-git@users.noreply.github.com>
2021-03-30 21:09:08 -0500

    metodo validaciones

--- src/controlador/Validaciones.java
+++ src/controlador/Validaciones.java
@@ -5,8 +5,11 @@
  */
 package controlador;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.JTextField;
 
 /**
  *
@@ -28,5 +31,38 @@ public class Validaciones {
     protected static boolean validarCorreo(String correo) {
         Matcher matcher = P_CORREO.matcher(correo);
         return matcher.matches();
+    }
+     public void ValidarLetras(JTextField campo) {
+        campo.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (Character.isDigit(c)) {
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void ValidarNumeros(JTextField campo) {
+        campo.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c)) {
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void LimitarCaracteres(JTextField campo, int cant) {
+        campo.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                int tamaño = campo.getText().length();
+                if (tamaño >= cant) {
+                    e.consume();
+                }
+            }
+        });
     }
 }
